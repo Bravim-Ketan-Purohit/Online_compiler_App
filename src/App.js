8c09e93f200b5c3d10006b1989a858219f2a1528
Bravim-Ketan-Purohit <92784033+Bravim-Ketan-Purohit@users.noreply.github.com>
2022-11-25 13:26:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Compiler from "./Components/Compiler/Compiler";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Compiler />
+      </>
+    );
+  }
+}
